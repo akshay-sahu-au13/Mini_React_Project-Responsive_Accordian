4876648f0cc768e824f6dab869f033584240b739
Akshay Sahu <akki5233@gmail.com>
2021-04-17 00:56:34 +0530

    Creating a responsive accordian

---
+++ responsive_accordian/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Question from './Question';
+
+
+const App = ()=> {
+
+  return (
+    <main>
+      section.
+    </main>
+  )
+}
+
+
+
+export default App;
---
+++ responsive_accordian/src/Question.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+import { AiOutlinePlusCircle, AiOutlineMinusCircle } from "react-icons/ai";
+
+const Question = ()=> {
+
+    return <> 
+        <article className="item">
+        <h4></h4>
+
+        </article>
+    </>
+}
+
+
+export default Question;
